5eb1ef1702c3897e4e0f0d316fb491608b1058ae
Bibibush <rlawlsgus125@gmail.com>
2024-06-09 17:58:57 +0900

    sideBar color theming

--- src/components/pages/main/SideBar.tsx
+++ src/components/pages/main/SideBar.tsx
@@ -1,16 +1,52 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function SideBar() {
+  const [selectedMenuId, setSelectedMenuId] = useState<number | null>(null);
+
+  const menus = [
+    {
+      id: 1,
+      name: "제품",
+      Children: [],
+    },
+    {
+      id: 2,
+      name: "공지사항",
+      Children: [],
+    },
+    {
+      id: 3,
+      name: "문의",
+      Children: [],
+    },
+  ];
+
   return (
     <Flex
-      bg="gray"
+      bg="gray.300"
       direction="column"
+      gap={4}
       height="100%"
       overflow="hidden"
       position="fixed"
-      top={0}
+      pt="36px"
+      top="120px"
       left={0}
       width="260px"
-    ></Flex>
+    >
+      {menus.map((menu) => {
+        return (
+          <Button
+            colorScheme={menu.id === selectedMenuId ? "gray" : "purple"}
+            onClick={() => {
+              setSelectedMenuId(menu.id);
+            }}
+          >
+            {menu.name}
+          </Button>
+        );
+      })}
+    </Flex>
   );
 }
